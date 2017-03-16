1c33c97fc9598d148818b59df602ef8adfb2723b
Millie Walsh <millie@primeacademy.io>
2017-03-16 18:11:12 -0500

    more comments

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ var namesArray = ['Millie', 'Dev', 'Ben', 'Sean', 'Neota', 'Ryan'];
 // console.log(namesArray[0]); //Millie
 // console.log(namesArray[1]); //Dev
 
-// loops
+// loops - we can write in three lines what would have taken 6
+// using the above method.
 for (var i = 0; i < namesArray.length; i++) {
   console.log(appendBang(namesArray[i]));
 }
 
 // append or add ! to end of string pass into it
+// functions encapluate chucks of logic that can be reused anywhere in the program
 function appendBang(theString) {
   return theString + "!";
 }
